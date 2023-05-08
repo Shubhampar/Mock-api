a7a6c8749af1d0f3ab714100831ad88b67e8a77a
Shubhampar <119346050+Shubhampar@users.noreply.github.com>
2023-05-08 11:29:18 +0530

    push-2

--- Mock-api/server.js
+++ Mock-api/server.js
@@ -1,10 +1,10 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('', router)
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+})
